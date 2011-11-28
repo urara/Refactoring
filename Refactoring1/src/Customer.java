f19c153cda900c8ca17ea8a0cdfe6cfccffaa031
urara036 <oura036@gmail.com>
2011-11-28 22:21:30 +0900

    テスト

--- Refactoring1/src/Customer.java
+++ Refactoring1/src/Customer.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 public class Customer {
 	private String _name;
 	private Vector _rentals = new Vector();
-	
+	//ƒ‰ƒNƒKƒL
 	public Customer(String name){
 		_name = name;
 	}
