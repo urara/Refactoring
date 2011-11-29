885a202886dce2dcd6843c44d8bb2605cd6222c5
urara036 <oura036@gmail.com>
2011-11-29 20:25:26 +0900

    delete temporary valiable

--- Refactoring1/src/Customer.java
+++ Refactoring1/src/Customer.java
@@ -4,7 +4,7 @@ import java.util.Vector;
 
 public class Customer {
 	private String _name;
-	private Vector _rentals = new Vector();
+	private Vector<Rental> _rentals = new Vector<Rental>();
 	//ラクガキ
 	public Customer(String name){
 		_name = name;
@@ -18,27 +18,47 @@ public class Customer {
 	}
 	
 	public String statement(){
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Enumeration rentals = _rentals.elements();
+		//double totalAmount = 0;
+		//int frequentRenterPoints = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while(rentals.hasMoreElements()){
 			Rental each = (Rental)rentals.nextElement();
 	
 			//レンタルポイントを加算
-			frequentRenterPoints += each.getFrequentPoints();
+			//frequentRenterPoints += each.getFrequentPoints();
 			
-			//この貸し出しに関する数値の表示1
+			//この貸し出しに関する数値の表示
 			result += "\t" + each.getMovie().getTitle() + "\t" +
 					String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
+			//totalAmount += each.getCharge();
 			
 		}
 		
 		//フッタ追加
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + 
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + 
 				" frequent renter points";
+		return result;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while(rentals.hasMoreElements()){
+			Rental each = rentals.nextElement();
+			result += each.getFrequentPoints();
+		}
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()){
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getCharge();
+		}
 		return result;
 	}
 	
